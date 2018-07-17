4b7d34ab73b98fc75b2eb83e1b509ba93be91218
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-07-17 15:12:32 -0700

    Initial commit.

---
+++ twitterclonejs-frontend/components/Tweet.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class = "tweet-container">
+    <img src = "" class = "" alt = "">
+    <div>
+      <div>
+        <strong class = "name">Eric Liang</strong>
+        <p class = "username">@ericwliang</p>
+      </div>
+      <div>
+        <p class = "tweet-content">
+          Looking for a web developer? Hire me! I help small businesses and startups get their website up and running.
+        </p>
+      </div>
+      <ul class = "">
+        <li><a href = ""><i class = ""><3</i> 0 </a></li>
+      </ul>
+    </div>
+  </div><!--end tweet-container-->
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ twitterclonejs-frontend/components/TweetInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class = "tweet-container">
+    <img src = "" class = "" alt = "">
+    <div>
+      <textarea placeholder = "What's happening?" rows = "5" maxlength = "280"></textarea>
+      <div class = "">
+        <input type = "submit" value = "Tweet">
+      </div>
+    </div>
+  </div><!--end tweet-container-->
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style>
+
+</style>
