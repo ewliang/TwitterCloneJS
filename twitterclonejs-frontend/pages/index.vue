d06137dbe2dbdb80948dfeece78936f852331372
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-08-06 15:31:08 -0700

    Included Tweet and TweetInput components. Started setting up the general layout of the Twitter layout.

--- twitterclonejs-frontend/pages/index.vue
+++ twitterclonejs-frontend/pages/index.vue
@@ -1,44 +1,29 @@
 <template>
-  <section class="container">
-    <div>
-      <h1 class="title">
-        Twitter Clone
-      </h1>
-      <h2 class="subtitle">
-        The frontend interface for the Twitter Clone JS project by Eric Liang.
-      </h2>
+  <div class = "container-fluid">
+    <div class = "">
+      <div class = "sidebar">
+      </div><!--end sidebar-->
+      <main>
+        <TweetInput></TweetInput>
+        <Tweet></Tweet>
+      </main>
     </div>
-  </section>
+  </div><!--end container-fluid-->
 </template>
 
 <script>
+import TweetInput from '~/components/TweetInput.vue'
+import Tweet from '~/components/Tweet.vue'
+
 export default {
+  components: {
+    TweetInput, Tweet
+  },
+  async asyncData () {
+
+  }
 }
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
 </style>
