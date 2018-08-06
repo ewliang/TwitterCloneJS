f05f65d8bc4df536db0d98cb5be90af3c2494061
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-08-06 15:30:12 -0700

    Minor HTML refactorization and styling.

--- twitterclonejs-frontend/components/TweetInput.vue
+++ twitterclonejs-frontend/components/TweetInput.vue
@@ -1,12 +1,12 @@
 <template>
   <div class = "tweet-container">
-    <img src = "" class = "" alt = "">
-    <div>
-      <textarea placeholder = "What's happening?" rows = "5" maxlength = "280"></textarea>
-      <div class = "">
-        <input type = "submit" value = "Tweet">
+    <img src = "" class = "circular" alt = "ProfilePicture">
+    <form class = "">
+      <textarea class = "row tweet-textarea" placeholder = "What's happening?" rows = "5"></textarea>
+      <div class = "row">
+        <input type = "submit" class = "btn btn-primary" value = "Tweet">
       </div>
-    </div>
+    </form>
   </div><!--end tweet-container-->
 </template>
 
@@ -21,7 +21,7 @@ export default {
   },
   methods: {
     createTweet () {
-      if(this.tweet.length > 280 || this.tweet.length == 0 || this.tweet == null) {
+      if(this.tweet.length > 280 || this.tweet.length == 0) {
         console.log('Invalid Tweet')
       } else {
         console.log('Valid Tweet')
@@ -32,6 +32,18 @@ export default {
 
 </script>
 
-<style>
+<style scoped>
+.tweet-container {
+  width: 100%;
+  max-width: 100%;
+}
 
+.tweet-textarea {
+  border: 1px solid #ABE6F8;
+  border-radius: 5px;
+}
+
+.tweet-textarea:focus {
+  border: 2px solid #ABE6F8;
+}
 </style>
