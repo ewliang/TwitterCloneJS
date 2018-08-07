ed2b6602b16723c40d0c0da4cf2880d7c2657d7c
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-08-07 13:45:32 -0700

    Created basic CRUD functions.

--- twitterclonejs-backend/controllers/tweetController.js
+++ twitterclonejs-backend/controllers/tweetController.js
@@ -1,8 +1,64 @@
+var Tweet = require('../models/Tweet');
+
 module.exports = {
-  //Get Tweet
-  getTweet: function(req, res) {
-    res.status(200).json({
-      message: "Welcome to the backend!"
+  getTweets: function(req, res) {
+    Tweet.find({}, (err, data) => {
+      if(err) {
+        res.status(404).json({
+          message: err
+        });
+      } else {
+        if(data == null) {
+          res.status(400).json({
+            message: "No tweets were found."
+          });
+        } else {
+          res.status(200).json(data);
+        }
+      }
+    });
+  },
+
+  getTweetsByUserID: function(req, res) {
+    Tweet.find({}, (err, data) => {
+      if(err) {
+        res.status(404).json({
+          message: err
+        });
+      } else {
+        if(data == null) {
+          res.status(400).json({
+            message: "No tweets were found."
+          });
+        } else {
+          res.status(200).json(data);
+        }
+      }
+    });
+  },
+
+  createTweet: function(req, res) {
+    var newTweetData = new Tweet({
+
+    });
+  },
+
+  deleteTweet: function(req, res) {
+    Tweet.findByIdAndRemove({ _id: req.params.id }, (err, data) => {
+      if(err) {
+        console.error(err);
+        res.status(404).json({
+          message: err
+        });
+      } else {
+        if(data == null) {
+          res.status(204).json({
+            message: "Failed to delete the requested tweet. The tweet was not found."
+          });
+        } else {
+          res.status(200).json(data);
+        }
+      }
     });
   }
 }
