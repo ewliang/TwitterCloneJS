ec0177e8c7b130a1ba148e92b58e14583f85a49a
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-08-09 19:05:58 -0700

    Initial commit.

---
+++ twitterclonejs-frontend/components/ProfileOverview-Public.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class = "container-fluid">
+    <div class = "row">
+      <img src = "" class = "img-fluid circular" alt = "">
+      <span class = "">Eric Liang</span>
+      <span class = "">@ericwliang</span>
+      <p class = "">
+
+      </p>
+      <date>Joined </date>
+    </div><!--end row-->
+  </div><!--end container-fluid-->
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ twitterclonejs-frontend/pages/_username.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class = "container-fluid">
+    <div>
+      <div class = "sidebar">
+      </div><!--end sidebar-->
+      <main>
+        <div v-for = "(tweet, index) in userTweets" v-bind:key = "index">
+          <TweetComponent v-bind:tweet = tweet.tweet v-bind:author = tweet.author></TweetComponent>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import TweetComponent from '~/components/Tweet.vue'
+
+export default {
+  async asyncData ({ params }) {
+    let userTweets = await axios.get('http://localhost:4000/tweets/${params.username}')
+    return {
+      userTweets: userTweets.data
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
